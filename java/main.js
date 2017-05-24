6620e7ad983c490c6969914b14d3324b0d94c7d1
Drew C <drewgamesaw@gmail.com>
2017-05-24 10:00:01 -0400

    Update main.js

--- java/main.js
+++ java/main.js
@@ -4,8 +4,8 @@ function start() {
     document.getElementById("nav").innerHTML = '<a id = "home" href = "index.html" class = "mainbtn" style = "width: 160px;height: 80px;">HOME</a> '+
         '<a id = "pg2" href = "page2.html" class = "mainbtn" style = "width: 160px;height: 80px;">Questions</a> '+
         '<a id = "bd" href = "bigdata.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Data</a> '+
-        '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a>'+
-        '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">pg4</a>'
+        '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a> '+
+        '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">Learned</a>'
     if(document.URL.endsWith('index.html')){ document.getElementById("home").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('page2.html')){ document.getElementById("pg2").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('bigdata.html')){ document.getElementById("bd").className = "mainbtn mainbtn_curpage"}
