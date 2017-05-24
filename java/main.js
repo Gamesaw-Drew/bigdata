d336109768b54111c7abd717aea7709c1c74adf1
Drew <drewgamesaw@gmail.com>
2017-05-24 16:26:57 -0400

    Visual Tweaks

--- java/main.js
+++ java/main.js
@@ -1,11 +1,17 @@
 function start() {
     document.getElementById("footerbase").innerHTML = "Bigdata";
-    if( document.getElementById("credits")) bigData1();
-    document.getElementById("nav").innerHTML = '<a id = "home" href = "index.html" class = "mainbtn" style = "width: 160px;height: 80px;">HOME</a> '+
-        '<a id = "pg2" href = "page2.html" class = "mainbtn" style = "width: 160px;height: 80px;">Questions</a> '+
-        '<a id = "bd" href = "bigdata.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Data</a> '+
-        '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a> '+
-        '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">Learned</a>'
+    if (document.getElementById("credits")) bigData1();
+    createNav();
+    setTimeout(setupSelectedBtn, 20);
+}
+function createNav(){document.getElementById("nav").innerHTML = '<a id = "home" href = "index.html" class = "mainbtn" style = "width: 160px;height: 80px;">HOME</a> '
+    document.getElementById("nav").innerHTML += '<a id = "pg2" href = "page2.html" class = "mainbtn" style = "width: 160px;height: 80px;">Questions</a> ';
+    document.getElementById("nav").innerHTML += '<a id = "bd" href = "bigdata.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Data</a> ';
+    document.getElementById("nav").innerHTML += '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a> ';
+    document.getElementById("nav").innerHTML += '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">Learned</a>';
+}
+function setupSelectedBtn() {
+
     if(document.URL.endsWith('index.html')){ document.getElementById("home").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('page2.html')){ document.getElementById("pg2").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('bigdata.html')){ document.getElementById("bd").className = "mainbtn mainbtn_curpage"}
@@ -14,7 +20,7 @@ function start() {
 }
 
 function begin() {
-    setTimeout(start, 50);  // Wait a few milliseconds to make sure all the objects it wants are loaded
+    setTimeout(start, 1);
 }
 
 function bigData1(){
