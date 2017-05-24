514664f2e8d48df88bf4ad7b66495728c2411d0f
Drew C <drewgamesaw@gmail.com>
2017-05-24 09:55:36 -0400

    Nav Bar

--- java/main.js
+++ java/main.js
@@ -5,6 +5,10 @@ function start() {
         '<a id = "pg2" href = "page2.html" class = "mainbtn" style = "width: 160px;height: 80px;">Questions</a> '+
         '<a id = "bd" href = "bigdata.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Data</a> '+
         '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a>'
+    if(document.URL.endsWith('page2.html')){ document.getElementById("pg2").className = "mainbtn mainbtn_curpage"}
+    if(document.URL.endsWith('bigdata.html')){ document.getElementById("bd").className = "mainbtn mainbtn_curpage"}
+    if(document.URL.endsWith('page3.html')){ document.getElementById("pg3").className = "mainbtn mainbtn_curpage"}
+    if(document.URL.endsWith('page4.html')){ document.getElementById("pg4").className = "mainbtn mainbtn_curpage"}
 }
 
 function begin() {
