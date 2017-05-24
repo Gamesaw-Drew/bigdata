13abd3235d81714d13105c02a7e8d0510cedcaa9
Drew C <drewgamesaw@gmail.com>
2017-05-24 09:58:28 -0400

    shh lets not talk about that

--- java/main.js
+++ java/main.js
@@ -5,7 +5,7 @@ function start() {
         '<a id = "pg2" href = "page2.html" class = "mainbtn" style = "width: 160px;height: 80px;">Questions</a> '+
         '<a id = "bd" href = "bigdata.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Data</a> '+
         '<a id = "pg3" href = "page3.html" class = "mainbtn" style = "width: 160px;height: 80px;">The Big Question</a>'+
-        '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">pg4</a>'+
+        '<a id = "pg4" href = "page4.html" class = "mainbtn" style = "width: 160px;height: 80px;">pg4</a>'
     if(document.URL.endsWith('index.html')){ document.getElementById("home").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('page2.html')){ document.getElementById("pg2").className = "mainbtn mainbtn_curpage"}
     if(document.URL.endsWith('bigdata.html')){ document.getElementById("bd").className = "mainbtn mainbtn_curpage"}
